ee94f4b2724fb17a351c2ae7cfe76830951dcccb
Donstesh <stephenshifoko@gmail.com>
2022-02-22 10:19:30 +0300

    auth tocken

--- js/les-1.js
+++ js/les-1.js
@@ -3,3 +3,6 @@ console.log("introduction2"); //
 console.log('I\'m Stephen and am learning js with Azron'); //escape with back slash to use '
 console.log("I'm Stephen"); //enclosing string in double qoutes ensure using '
 //console.log(introduction3);
+
+
+//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
