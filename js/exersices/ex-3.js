92b475ffe57d72b18687a3a78ba94fec766221f9
Donstesh <stephenshifoko@gmail.com>
2022-02-23 12:03:16 +0300

    while loop even to print even numbers

---
+++ js/exersices/ex-3.js
@@ -0,0 +1,11 @@
+//use a while loop to print even numbers between 40 and 50
+
+let number = 40;
+
+while(number < 50 ){
+    
+    if(number % 2 === 0 && number > 40){
+        console.log(number);        
+    }
+    number++;
+}
