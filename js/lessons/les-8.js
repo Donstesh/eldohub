dbd50476f395254c4b4268474317e46ea004504f
Donstesh <stephenshifoko@gmail.com>
2022-02-23 12:02:54 +0300

    while loop

---
+++ js/lessons/les-8.js
@@ -0,0 +1,24 @@
+// printing 1 to 10
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+let number = 10;
+//while loop
+
+while(number >= 1){
+    console.log(number);
+    number --;
+}
+
+console.log('loop endded');
+
+
