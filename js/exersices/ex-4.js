70778caf3396c52b0708ab08e024b2aa32ddd398
Donstesh <stephenshifoko@gmail.com>
2022-02-23 12:03:32 +0300

    while loop to print odd numbers

---
+++ js/exersices/ex-4.js
@@ -0,0 +1,12 @@
+//use while loop to print odd number between 1 - 10
+
+
+let number = 1;
+
+while(number < 10 ){
+    
+    if(number % 2 !== 0){
+        console.log(number);        
+    }
+    number++;
+}
